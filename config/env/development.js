b716ff466a65c357e6d71f9defcbb889e81dd441
mubeen unissa <mubeen@usmb000000.lan>
2019-07-04 19:25:25 -0500

    updated the mangodb connection string

--- config/env/development.js
+++ config/env/development.js
@@ -10,6 +10,7 @@ module.exports = {
    // If you want to use a MongoLab instance, just sign up for it, create a data-store, in this example we call it nodegoat.
    // and again just run the grunt db-reset:development command
    //db: 'mongodb://<dbuser>:<dbpassword>@<databasename>',
+   db: 'mongodb://nodegoat:owasp@ds159217.mlab.com:59217/nodegoat'
 
    // If you want to debug regression tests, you will need the following which is also in the test config:
    zapHostName: "192.168.56.20",
